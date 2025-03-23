d34999a56857a801e53837c4ad53506b750b5585
sairammedavaka <sairam.medavaka92@gmail.com>
2025-03-22 22:53:59 -0400

    Completed all exercises

--- constructor-exercise.js
+++ constructor-exercise.js
@@ -1,1 +1,26 @@
-/* Please read the instructions located in the 1-constructor-exercise-readme.md file and complete your code below */ 
+/* Please read the instructions located in the 1-constructor-exercise-readme.md file and complete your code below */
+
+function Book(title, author, year, genre) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.genre = genre;
+}
+
+Book.prototype.getSummary = function () {
+  return `${this.title}, ${this.year} and ${this.genre}`;
+};
+
+Book.prototype.isClassic = function () {
+  return this.year > 50 ? true : false;
+};
+
+Book.prototype.updateYear = function (newYear) {
+  this.year = newYear;
+  return `Publication year updated to ${newYear}`;
+};
+
+Book.prototype.changeGenre = function (newGenre) {
+  this.genre = newGenre;
+  return `Genre updated to ${newGenre}`;
+};
--- oop-exercise.js
+++ oop-exercise.js
@@ -1,1 +1,59 @@
 /* Please read the instructions located in the exercises/2-oop-exercise-readme.md file and complete your code below */
+
+class Student {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.grades = [];
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getAge() {
+    return this.age;
+  }
+
+  addGrade(grade) {
+    if (grade < 0 || grade > 100) {
+      console.log("Grade must be between 0 and 100.");
+    } else {
+      this.grades.push(grade);
+    }
+  }
+
+  calculateAverageGrade() {
+    if (this.grades.length === 0) {
+      return 0;
+    }
+    return this.grades.reduce((acc, val) => acc + val, 0) / this.grades.length;
+  }
+
+  hasPassed(passingGrade) {
+    const avgVal = this.calculateAverageGrade();
+    return avgVal >= passingGrade ? true : false;
+  }
+}
+
+class Course {
+  constructor(title) {
+    this.title = title;
+    this.students = [];
+  }
+
+  enrollStudent(student) {
+    this.students.push(student);
+  }
+
+  calculateCourseAverage() {
+    if (this.students.length === 0) {
+      return 0;
+    }
+    const totalVal = this.students.reduce(
+      (acc, val) => acc + val.calculateAverageGrade(),
+      0
+    );
+    return totalVal / this.students.length;
+  }
+}
